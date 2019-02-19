e0c9218b1abbb71cb1412b3cab2be2bbe237e4fb
Roo Williams <roo@roowilliams.com>
2019-02-18 20:47:55 -0500

    add creds for user

--- server.js
+++ server.js
@@ -27,44 +27,44 @@ const userActivityWebhook = twitterWebhooks.userActivity({
   app
 })
 
-//Register your webhook url - just needed once per URL
+// Register your webhook url - just needed once per URL
 userActivityWebhook.register().catch(err => console.log)
 
 // Subscribe for a particular user activity
 
-// userActivityWebhook
-//   .subscribe({
-//     userId: '[TWITTER USER ID]',
-//     accessToken: '[TWITTER USER ACCESS TOKEN]',
-//     accessTokenSecret: '[TWITTER USER ACCESS TOKEN SECRET]'
-//   })
-//   .then(function(userActivity) {
-//     userActivity
-//       .on('favorite', data => console.log(userActivity.id + ' - favorite'))
-//       .on('tweet_create', data =>
-//         console.log(userActivity.id + ' - tweet_create')
-//       )
-//       .on('follow', data => console.log(userActivity.id + ' - follow'))
-//       .on('mute', data => console.log(userActivity.id + ' - mute'))
-//       .on('revoke', data => console.log(userActivity.id + ' - revoke'))
-//       .on('direct_message', data =>
-//         console.log(userActivity.id + ' - direct_message')
-//       )
-//       .on('direct_message_indicate_typing', data =>
-//         console.log(userActivity.id + ' - direct_message_indicate_typing')
-//       )
-//       .on('direct_message_mark_read', data =>
-//         console.log(userActivity.id + ' - direct_message_mark_read')
-//       )
-//       .on('tweet_delete', data =>
-//         console.log(userActivity.id + ' - tweet_delete')
-//       )
-//   })
+userActivityWebhook
+  .subscribe({
+    userId: 'roowilliams',
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+  .then(function(userActivity) {
+    userActivity
+      .on('favorite', data => console.log(userActivity.id + ' - favorite'))
+      .on('tweet_create', data =>
+        console.log(userActivity.id + ' - tweet_create')
+      )
+      .on('follow', data => console.log(userActivity.id + ' - follow'))
+      .on('mute', data => console.log(userActivity.id + ' - mute'))
+      .on('revoke', data => console.log(userActivity.id + ' - revoke'))
+      .on('direct_message', data =>
+        console.log(userActivity.id + ' - direct_message')
+      )
+      .on('direct_message_indicate_typing', data =>
+        console.log(userActivity.id + ' - direct_message_indicate_typing')
+      )
+      .on('direct_message_mark_read', data =>
+        console.log(userActivity.id + ' - direct_message_mark_read')
+      )
+      .on('tweet_delete', data =>
+        console.log(userActivity.id + ' - tweet_delete')
+      )
+  })
 
 // listen to any user activity
-userActivityWebhook.on('event', (event, userId, data) =>
-  console.log(userId + ' - favorite')
-)
+// userActivityWebhook.on('event', (event, userId, data) =>
+//   console.log(userId + ' - favorite')
+// )
 
 // listen to unknown payload (in case of api new features)
 userActivityWebhook.on('unknown-event', rawData => console.log(rawData))
